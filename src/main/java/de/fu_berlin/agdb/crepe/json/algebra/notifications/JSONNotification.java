e30c0bb872c805e7a9867e325a61e14baeb945ce
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-10-27 13:19:57 +0100

    Add subclass relationship for JSONIonicPushNotification

--- src/main/java/de/fu_berlin/agdb/crepe/json/algebra/notifications/JSONNotification.java
+++ src/main/java/de/fu_berlin/agdb/crepe/json/algebra/notifications/JSONNotification.java
@@ -9,7 +9,7 @@ import de.fu_berlin.agdb.crepe.json.algebra.JSONOperator;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@class")
-@JsonSubTypes({@JsonSubTypes.Type(JSONVerboseNotification.class)})
+@JsonSubTypes({@JsonSubTypes.Type(JSONVerboseNotification.class), @JsonSubTypes.Type(JSONIonicPushNotification.class)})
 public abstract class JSONNotification<T extends Notification> extends JSONAlgebraElement<T> {
     protected JSONOperator<?> rule;
     // Let's ignore this for now
